1e6b701124a5e90f20685dfcd20549b5f5b9c389
AkshayBadekar-fpl <78082756+AkshayBadekar-fpl@users.noreply.github.com>
2021-08-18 17:44:00 +0530

    added

--- back/taiga_contrib_proxy_auth/services.py
+++ back/taiga_contrib_proxy_auth/services.py
@@ -96,10 +96,10 @@ def proxy_login_func(request):
 
 
 root = logging.getLogger()
-root.setLevel(logging.DEBUG)
+root.setLevel(logging.INFO)
 
 handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.DEBUG)
+handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 root.addHandler(handler)
